e46bbb8a5bbb9b552ba773f993623768c40689f9
semen <semehvor@gmail.com>
2024-03-21 12:36:03 +0300

    eslint fixes

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import MainScreen from './pages/main-screen/main-screen';
 import { AppRoute, AuthorizationStatus } from './const';
 import FavoritesScreen from './pages/favourites-screen/favourites-screen';
@@ -6,7 +6,6 @@ import LoginScreen from './pages/login-screen/login-screen';
 import OfferScreen from './pages/offer-screen/offer-screen';
 import PrivateRoute from './components/private-route/private-route';
 import NotFoundScreen from './pages/not-found-screen/not-found-screen';
-
 
 type AppScreenProps = {
   offerCardNumber: number;
@@ -16,35 +15,22 @@ export default function App({ offerCardNumber }: AppScreenProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-      <Route
-        path={AppRoute.Root}
-        element={<MainScreen offerCardNumber={offerCardNumber} />}
-      />
-      <Route
-        path={AppRoute.Login}
-        element={<LoginScreen />}
-      />
-      <Route
-        path={AppRoute.Offer}
-        element={<OfferScreen />}
-      />
-      <Route
-            path={AppRoute.Favourites}
-            element={
-              <PrivateRoute
-                authorizationStatus={AuthorizationStatus.NoAuth}
-              >
-                <FavoritesScreen />
-              </PrivateRoute>
-            }
-        />
         <Route
-            path="*"
-            element={<NotFoundScreen />}
+          path={AppRoute.Root}
+          element={<MainScreen offerCardNumber={offerCardNumber} />}
         />
+        <Route path={AppRoute.Login} element={<LoginScreen />} />
+        <Route path={AppRoute.Offer} element={<OfferScreen />} />
+        <Route
+          path={AppRoute.Favourites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFoundScreen />} />
       </Routes>
     </BrowserRouter>
-  )
-
-
+  );
 }
